6febccfd4fb9d24da429d25aeea96baef5eb41d4
matus.lackovic <matus_l@abakus.com>
2017-10-09 16:55:54 +0200

    Added CoreTenant object for communicating with core instead of standalone functions.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let fioAccountStatement = require("./fio-response-example.json");
 let fio = require("./fio.js");
 let core = require("./core.js");
-let sync = require("./sync");
+let sync = require("./sync.js");
 
 async function main() {
   let coreAccountStatement = fio.extractCoreAccountStatement(fioAccountStatement);
   let accounts = fio.extractUniqueCoreAccounts(fioAccountStatement);
 
-  await core.createMissingAccounts(accounts);
-  await core.createTransactions(coreAccountStatement.transactions);
+  let tenantJohny = new core.CoreTenant("http://localhost:8080", "johny");
+  await tenantJohny.createMissingAccounts(accounts);
+  await tenantJohny.createTransactions(coreAccountStatement.transactions);
 
   await sync.updateTransactionCheckpoint("./db.json", coreAccountStatement.accountNumber, coreAccountStatement.idTransactionTo);
 }
@@ -18,5 +19,6 @@ main()
   .then(() => console.log("FINISH - Everything went well"))
   .catch(error => {
     console.log(error);
+    console.log(error.message);
     console.log("There were some unexpected error see above");
   });
--- core.js
+++ core.js
@@ -1,14 +1,27 @@
 let axios = require("axios");
 
-async function createMissingAccounts(accounts) {
+function CoreTenant (coreHost, tenant) {
+  if (!coreHost || !tenant) {
+    throw Error("When creating CoreTenant you have to provide both parameters - coreHost and tenant");
+  }
+  this._coreHost = coreHost;
+  this._tenant = tenant;
+}
+
+CoreTenant.prototype._getApiUrl = function () {
+  return this._coreHost + "/v1/" + this._tenant + "/core";
+};
+
+CoreTenant.prototype.createMissingAccounts = async function (accounts) {
+  console.log(this._getApiUrl());
   await Promise.all(accounts.map(
     async account => {
       try {
-        await axios.get("http://localhost:8080/v1/johny/core/account/" + account.accountNumber);
+        await axios.get(this._getApiUrl() + "/account/" + account.accountNumber);
         console.log("Account " + account.accountNumber + " already exists");
       } catch (error) {
         if (error.response && error.response.status === 404) {
-          await axios.put("http://localhost:8080/v1/johny/core/account/", account);
+          await axios.put(this._getApiUrl() + "/account/", account);
           console.log("Created account " + account.accountNumber);
         } else {
           throw error;
@@ -16,18 +29,17 @@ async function createMissingAccounts(accounts) {
       }
     }
   ));
-}
+};
 
-async function createTransactions(transactions) {
+CoreTenant.prototype.createTransactions = async function (transactions) {
   await Promise.all(transactions.map(
     async (transaction, index) => {
-      await axios.put("http://localhost:8080/v1/johny/core/transaction", transaction);
+      await axios.put(this._getApiUrl() + "/transaction", transaction);
       console.log("Created " + (index+1) + ". transaction");
     }
   ));
-}
+};
 
 module.exports = {
-  createMissingAccounts,
-  createTransactions
-}
+  CoreTenant
+};
