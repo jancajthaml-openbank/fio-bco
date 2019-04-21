facf2c6c3744dc029104f102df04b42f20a5cda4
Jan Cajthaml <jan.cajthaml@gmail.com>
2019-04-21 20:07:24 +0200

    better http helper in bbtests

--- bbtest/steps/token_steps.rb
+++ bbtest/steps/token_steps.rb
@@ -6,27 +6,17 @@ step "token :slash_pair is created" do |slash_pair|
 
   (tenant, token) = slash_pair.split('/')
 
-  send "I request curl :http_method :url", "POST", "https://localhost/token/#{tenant}", {
-    "username": "X",
-    "password": "Y"
+  uri = "https://127.0.0.1/token/#{tenant}"
+
+  payload = {
+    value: "X",
   }.to_json
 
-  @resp = { :code => 0 }
+  send "I request curl :http_method :url", "POST", uri, payload
+  send "curl responds with :http_status", 200
 
-  eventually(timeout: 60, backoff: 2) {
-    resp = %x(#{@http_req})
-    @resp[:code] = resp[resp.length-3...resp.length].to_i
-
-    if @resp[:code] === 0
-      raise "endpoint #{@http_req} is unreachable"
-    end
-
-    @resp[:body] = resp[0...resp.length-3] unless resp.nil?
-  }
-
-  resp = JSON.parse(@resp[:body])
-
-  @tokens[tenant+"/"+token] = resp["value"] if @resp[:code] == 200
+  token_value = JSON.parse(HTTPHelper.response[:body])["value"]
+  @tokens[tenant+"/"+token] = token_value
 end
 
 step "token :slash_pair should exist" do |slash_pair|
@@ -37,32 +27,20 @@ step "token :slash_pair should exist" do |slash_pair|
   (tenant, token) = slash_pair.split('/')
   token_value = @tokens[slash_pair]
 
-  send "I request curl :http_method :url", "GET", "https://localhost/token/#{tenant}"
+  uri = "https://127.0.0.1/token/#{tenant}"
 
-  @resp = { :code => 0 }
+  send "I request curl :http_method :url", "GET", uri
+  send "curl responds with :http_status", 200
 
-  eventually(timeout: 60, backoff: 2) {
-    resp = %x(#{@http_req})
-    @resp[:code] = resp[resp.length-3...resp.length].to_i
-
-    if @resp[:code] === 0
-      raise "endpoint #{@http_req} is unreachable"
-    end
-
-    @resp[:body] = resp[0...resp.length-3] unless resp.nil?
-  }
-
-  send "request should succeed"
-
-  actual_tokens = JSON.parse(@resp[:body]).map { |item| tenant + "/" + item }
+  actual_tokens = JSON.parse(HTTPHelper.response[:body]).map { |item| tenant + "/" + item }
 
   expect(actual_tokens).to include(tenant+"/"+token_value), "expected to find \"#{slash_pair}\" in #{actual_tokens}"
 end
 
 step "request should succeed" do ||
-  expect(@resp[:code]).to eq(200), "actual response #{@resp}"
+  expect(HTTPHelper.response[:code]).to eq(200), "expected 200 got\n#{HTTPHelper.response[:raw]}"
 end
 
 step "request should fail" do ||
-  expect(@resp[:code]).to_not eq(200), "actual response #{@resp}"
+  expect(HTTPHelper.response[:code]).to_not eq(200), "expected non 200 got\n#{HTTPHelper.response[:raw]}"
 end
