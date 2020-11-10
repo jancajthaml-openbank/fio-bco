01b629e510444129b3e5e570e5c68ac0a5cdf4c3
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-11-10 09:47:36 +0100

    increase ut coverage

--- services/fio-bco-import/boot/init.go
+++ services/fio-bco-import/boot/init.go
@@ -85,9 +85,9 @@ func Initialize() Program {
 	)
 
 	var daemons = make([]utils.Daemon, 0)
-	daemons = append(daemons, metricsDaemon)
-	daemons = append(daemons, actorSystemDaemon)
-	daemons = append(daemons, fioDaemon)
+	daemons = append(daemons, &metricsDaemon)
+	daemons = append(daemons, &actorSystemDaemon)
+	daemons = append(daemons, &fioDaemon)
 
 	return Program{
 		interrupt: make(chan os.Signal, 1),
--- services/fio-bco-rest/boot/init.go
+++ services/fio-bco-rest/boot/init.go
@@ -84,12 +84,12 @@ func Initialize() Program {
 	)
 
 	var daemons = make([]utils.Daemon, 0)
-	daemons = append(daemons, metricsDaemon)
-	daemons = append(daemons, actorSystemDaemon)
-	daemons = append(daemons, restDaemon)
-	daemons = append(daemons, systemControlDaemon)
-	daemons = append(daemons, diskMonitorDaemon)
-	daemons = append(daemons, memoryMonitorDaemon)
+	daemons = append(daemons, &metricsDaemon)
+	daemons = append(daemons, &actorSystemDaemon)
+	daemons = append(daemons, &restDaemon)
+	daemons = append(daemons, &systemControlDaemon)
+	daemons = append(daemons, &diskMonitorDaemon)
+	daemons = append(daemons, &memoryMonitorDaemon)
 
 	return Program{
 		interrupt: make(chan os.Signal, 1),
