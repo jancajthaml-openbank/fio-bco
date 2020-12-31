69e4ded61860704250ebdb1b4b8740755cb29ed6
Jan Cajthaml <jan.cajthaml@gmail.com>
2020-12-31 12:52:13 +0100

    upgrade actor system

--- bbtest/steps/orchestration_steps.py
+++ bbtest/steps/orchestration_steps.py
@@ -19,40 +19,36 @@ def step_impl(context, package, operation):
     execute(['systemctl', 'stop', package])
     (code, result, error) = execute(["apt-get", "-y", "remove", package])
     assert code == 0, "unable to uninstall with code {} and {} {}".format(code, result, error)
+    (code, result, error) = execute(["apt-get", "-y", "purge", package])
+    assert code == 0, "unable to purge with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/fio-bco/conf.d/init.conf') is False
   else:
-    assert False
+    assert False, 'unknown operation {}'.format(operation)
 
 
 @given('systemctl contains following active units')
 @then('systemctl contains following active units')
 def step_impl(context):
-  (code, result, error) = execute(["systemctl", "list-units", "--no-legend"])
-  assert code == 0
-
+  (code, result, error) = execute(["systemctl", "list-units", "--no-legend", "--state=active"])
+  assert code == 0, str(result) + ' ' + str(error)
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
-
   result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
   result = [item for item in result if item in items]
-
   assert len(result) > 0, 'units not found\n{}'.format(result)
 
 
 @given('systemctl does not contain following active units')
 @then('systemctl does not contain following active units')
 def step_impl(context):
-  (code, result, error) = execute(["systemctl", "list-units", "--no-legend"])
-  assert code == 0
-
+  (code, result, error) = execute(["systemctl", "list-units", "--no-legend", "--state=active"])
+  assert code == 0, str(result) + ' ' + str(error)
   items = []
   for row in context.table:
     items.append(row['name'] + '.' + row['type'])
-
   result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
   result = [item for item in result if item in items]
-
   assert len(result) == 0, 'units found\n{}'.format(result)
 
 
@@ -62,8 +58,8 @@ def unit_running(context, unit):
   @eventually(10)
   def wait_for_unit_state_change():
     (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
-    assert code == 0, code
-    assert 'SubState=running' in result, result
+    assert code == 0, str(result) + ' ' + str(error)
+    assert 'SubState=running' in result, str(result) + ' ' + str(error)
 
   wait_for_unit_state_change()
 
@@ -73,18 +69,19 @@ def unit_running(context, unit):
 @given('unit "{unit}" is not running')
 @then('unit "{unit}" is not running')
 def unit_not_running(context, unit):
-  (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
-  assert code == 0, str(result) + ' ' + str(error)
-  assert 'SubState=running' not in result, str(result) + ' ' + str(error)
+  @eventually(10)
+  def wait_for_unit_state_change():
+    (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
+    assert code == 0, str(result) + ' ' + str(error)
+    assert 'SubState=running' not in result, str(result) + ' ' + str(error)
 
+  wait_for_unit_state_change()
 
 @given('{operation} unit "{unit}"')
 @when('{operation} unit "{unit}"')
 def operation_unit(context, operation, unit):
   (code, result, error) = execute(["systemctl", operation, unit])
   assert code == 0, str(result) + ' ' + str(error)
-  if operation == 'restart':
-    unit_running(context, unit)
 
 
 @given('{unit} is configured with')
@@ -94,32 +91,20 @@ def unit_is_configured(context, unit):
     params[row['property']] = row['value']
   context.unit.configure(params)
 
-  (code, result, error) = execute([
-    'systemctl', 'list-units', '--no-legend'
-  ])
-  result = [item.split(' ')[0].strip() for item in result.split(os.linesep)]
-  result = [item for item in result if ("{}-".format(unit) in item and ".service" in item)]
 
-  for unit in result:
-    operation_unit(context, 'restart', unit)
-
-
+@then('tenant {tenant} is offboarded')
 @given('tenant {tenant} is offboarded')
 def offboard_unit(context, tenant):
   logfile = os.path.realpath('{}/../../reports/blackbox-tests/logs/fio-bco-import.{}.log'.format(os.path.dirname(__file__), tenant))
-
   (code, result, error) = execute(['journalctl', '-o', 'cat', '-u', 'fio-bco-import@{}.service'.format(tenant), '--no-pager'])
   if code == 0 and result:
     with open(logfile, 'w') as f:
       f.write(result)
-
   execute(['systemctl', 'stop', 'fio-bco-import@{}.service'.format(tenant)])
-
   (code, result, error) = execute(['journalctl', '-o', 'cat', '-u', 'fio-bco-import@{}.service'.format(tenant), '--no-pager'])
   if code == 0 and result:
     with open(logfile, 'w') as fd:
       fd.write(result)
-
   execute(['systemctl', 'disable', 'fio-bco-import@{}.service'.format(tenant)])
   unit_not_running(context, 'fio-bco-import@{}'.format(tenant))
 
@@ -128,8 +113,6 @@ def offboard_unit(context, tenant):
 def onboard_unit(context, tenant):
   (code, result, error) = execute(["systemctl", 'enable', 'fio-bco-import@{}'.format(tenant)])
   assert code == 0, str(result) + ' ' + str(error)
-
   (code, result, error) = execute(["systemctl", 'start', 'fio-bco-import@{}'.format(tenant)])
   assert code == 0, str(result) + ' ' + str(error)
-
   unit_running(context, 'fio-bco-import@{}'.format(tenant))
