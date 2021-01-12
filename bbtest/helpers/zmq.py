ad4bdcfd3b951e4e59eea3898b8b0498d9addfb7
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-01-12 20:40:27 +0100

    improve messages reliability

--- bbtest/helpers/zmq.py
+++ bbtest/helpers/zmq.py
@@ -45,12 +45,15 @@ class ZMQHelper(threading.Thread):
           continue
         self.backlog.append(data)
         self.__pub.send(data)
+        self.__pub.send(data)
       except Exception as ex:
         if ex.errno != 11:
           return
 
   def send(self, data):
-    self.__pub.send(data.encode())
+    payload = data.encode()
+    self.__pub.send(payload)
+    self.__pub.send(payload)
 
   def ack(self, data):
     self.__mutex.acquire()
