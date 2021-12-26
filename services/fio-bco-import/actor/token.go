0980016e2170bdb98769ef2d5b43280234a1ef1f
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-12-26 06:31:39 +0100

    logging cleanup

--- services/fio-bco-import/actor/token.go
+++ services/fio-bco-import/actor/token.go
@@ -49,7 +49,7 @@ func NonExistToken(s *System, id string) system.ReceiverFunction {
 			err := persistence.CreateToken(s.EncryptedStorage, id, msg.Value)
 			if err != nil {
 				s.SendMessage(FatalError, context.Sender, context.Receiver)
-				log.Debug().Err(err).Msgf("token %s (NonExist CreateToken) Error %s", id)
+				log.Debug().Err(err).Msgf("token %s (NonExist CreateToken) Error", id)
 				return NonExistToken(s, id)
 			}
 
